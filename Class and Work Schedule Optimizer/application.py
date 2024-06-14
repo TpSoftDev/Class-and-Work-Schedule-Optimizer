80ba051c3cbc83bdf15b352cdae809a86998400c
Thabang <tp.softwaredev@gmail.com>
2024-06-14 12:41:23 -0500

    Added Json Data to the AvailabilityUpdater.py

--- Class and Work Schedule Optimizer/application.py
+++ Class and Work Schedule Optimizer/application.py
@@ -10,6 +10,7 @@ from utils.helperFunctions import convert_to_readable_time, quicksort_shifts, fo
 from api_calls.schedule_source_api.schedule_source_api import getScheduleId, getEmptyShiftsForDay, getLocations, getScheduleNames
 from availabilityCalculator.main import filterEmptyShiftsForDay
 
+
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
--- Class and Work Schedule Optimizer/models/AvailabilityUpdater.py
+++ Class and Work Schedule Optimizer/models/AvailabilityUpdater.py
@@ -1,1 +1,57 @@
 ############################################ Update Availability in Schedule Source ####################################
+#Schedule Source data
+# Define the employee availability data
+employee_availability = [
+    {
+        "DayId": 1,
+        "AvailableRanges": "5am-11am;1:45pm-5pm;6:15pm-11:15pm;",
+        "LastName": "Abbasani",
+        "FirstName": "Hari Preetham Reddy (Preetham)",
+        "EmployeeExternalId": 170601496
+    },
+    {
+        "DayId": 2,
+        "AvailableRanges": "5am-9am",
+        "LastName": "Abbasani",
+        "FirstName": "Hari Preetham Reddy (Preetham)",
+        "EmployeeExternalId": 170601496
+    },
+    {
+        "DayId": 3,
+        "AvailableRanges": "12:01am-11:59pm",
+        "LastName": "Abbasani",
+        "FirstName": "Hari Preetham Reddy (Preetham)",
+        "EmployeeExternalId": 170601496
+    },
+    {
+        "DayId": 4,
+        "AvailableRanges": None,
+        "LastName": "Abbasani",
+        "FirstName": "Hari Preetham Reddy (Preetham)",
+        "EmployeeExternalId": 170601496
+    },
+    {
+        "DayId": 5,
+        "AvailableRanges": None,
+        "LastName": "Abbasani",
+        "FirstName": "Hari Preetham Reddy (Preetham)",
+        "EmployeeExternalId": 170601496
+    },
+    {
+        "DayId": 6,
+        "AvailableRanges": None,
+        "LastName": "Abbasani",
+        "FirstName": "Hari Preetham Reddy (Preetham)",
+        "EmployeeExternalId": 170601496
+    },
+    {
+        "DayId": 7,
+        "AvailableRanges": None,
+        "LastName": "Abbasani",
+        "FirstName": "Hari Preetham Reddy (Preetham)",
+        "EmployeeExternalId": 170601496
+    }
+]
+
+# Print the stored data to the terminal
+print(employee_availability)
